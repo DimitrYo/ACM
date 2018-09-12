0a8e6a6f79dcd1d8f073b70d065cdee4f33f1b99
Dymytr Yovchev <Dymytr.Yovchev@simcorp.com>
2018-09-12 17:51:14 +0300

    Small update.

--- ACM.BL2/Order.cs
+++ ACM.BL2/Order.cs
@@ -25,7 +25,7 @@ namespace ACM.BL {
             return logString;
         }
 
-        public bool Validate() {
+        public override bool Validate() {
             var isValid = true;
             if (OrderDate == null) isValid = false;
 
---
+++ Acme.Common.Test/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Common;
+using System.Linq;
+
+namespace Acme.Common.Test {
+    [TestClass]
+    public class StringHandlerTest {
+        [TestMethod]
+        public void TestMethod1() {
+            // Arrange
+            string source = "SonicScrewdriver";
+            var expected = "Sonic Screwdriver";
+
+            // Act
+            var actual = source.InsertSpaces();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void TestMethod2() {
+            // Arrange
+            var source = "Sonic Screwdriver";
+            var expected = "Sonic Screwdriver";
+
+            // Act
+            var actual = source.InsertSpaces();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void TestMethod3() {
+            // Arrange
+            var source = "ABC";
+            var expected = "A B C";
+
+            // Act
+            var actual = source.InsertSpaces();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
